8c548c2dbadfc37cf84dfb40ab82e4fb792706d5
Cromagne <44948050+Cromagne@users.noreply.github.com>
2019-05-04 12:37:00 +0200

    "Script python ajouté à github"

---
+++ script_test_python.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+#Ceci est un court script python qui affiche la table de multiplication
+#de 1 à 10 d'un nombre choisi par l'utilisateur
+#Je n'ai pas encore créé le repo sur Github. Je m'en occupe tout de suite
+#Puis j'écris le programme.
